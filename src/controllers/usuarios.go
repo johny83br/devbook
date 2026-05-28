67fbb26875d60c21b5948cdfe7a839850de347da
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-05-27 23:50:10 -0300

    created repositories of `usuarios`

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -1,9 +1,51 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuário"))
+	
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		http.Error(w, "Erro ao ler o corpo da requisição", http.StatusBadRequest)
+		return
+	}
+
+	var usuario models.Usuario
+	err := json.Unmarshal(corpoRequisicao, &usuario)
+	if err != nil {
+		http.Error(w, "Erro ao decodificar o corpo da requisição", http.StatusBadRequest)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		http.Error(w, "Erro ao conectar ao banco de dados", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositories.NovoRepositorioDeUsuarios(db)
+	_, erro = repositorio.Criar(usuario)
+	if erro != nil {
+		http.Error(w, "Erro ao criar usuário", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	if err := json.NewEncoder(w).Encode(usuario); err != nil {
+		http.Error(w, "Erro ao converter o usuário para JSON", http.StatusInternalServerError)
+		return
+	}
+
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
---
+++ src/repositories/usuarios.go
@@ -0,0 +1,34 @@
+package repositories
+
+import (
+	"api/src/models"
+	"database/sql"
+)
+
+type usuarios struct {
+	db *sql.DB
+}
+
+func NovoRepositorioDeUsuarios(db *sql.DB) *usuarios {
+	return &usuarios{db}
+}
+
+func (u usuarios) Criar(usuario models.Usuario) (uint64, error) {
+	statement, erro := u.db.Prepare("INSERT INTO usuarios (nome, nick, email, senha) VALUES (?, ?, ?, ?)")
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
+	if erro != nil {
+		return 0, erro
+	}
+
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIDInserido), nil
+}
