1b65b9a55b7658d754256444a82e6be1275f6b76
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-05-28 16:09:55 -0300

    added filter and format

--- src/models/Usuario.go
+++ src/models/Usuario.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Usuario struct {
 	ID 				uint64 			`json:"id,omitempty"`
@@ -9,4 +13,42 @@ type Usuario struct {
 	Email 		string 			`json:"email,omitempty"`
 	Senha 		string 			`json:"senha,omitempty"`
 	CriadoEm 	time.Time 	`json:"criadoEm,omitempty"`
+}
+
+func (u *Usuario) Preparar() error {
+	if err := u.validar(); err != nil {
+		return err
+	}
+
+	u.formatar()
+
+	return nil
+}
+
+func (u *Usuario) validar() error {
+
+	if u.Nome == "" {
+		return errors.New("O nome é obrigatório e não pode ser vazio")
+	}
+
+	if u.Nick == "" {
+		return errors.New("O nickname é obrigatório e não pode ser vazio")
+	}
+
+	if u.Email == "" {
+		return errors.New("O email é obrigatório e não pode ser vazio")
+	}
+
+	if u.Senha == "" {
+		return errors.New("A senha é obrigatória e não pode ser vazio")
+	}
+
+	return nil
+}
+
+func (u *Usuario) formatar() {
+	u.Nome = strings.TrimSpace(u.Nome)
+	u.Nick = strings.TrimSpace(u.Nick)
+	u.Email = strings.TrimSpace(u.Email)
+	u.Senha = strings.TrimSpace(u.Senha)
 }
