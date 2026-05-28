0291ced6f30ac850a382c11d1d91d275dfc0c641
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-05-27 22:05:47 -0300

    create routes and controllers of users

---
+++ src/router/rotas/usuarios.go
@@ -0,0 +1,39 @@
+package rotas
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var rotasUsuarios = []Rota{
+	{
+		URI:                "/api/usuarios",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.CriarUsuario,
+		RequerAutenticacao: false,
+	},
+	{
+		URI:                "/api/usuarios",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuarios,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/api/usuarios/{usuarioId}",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuario,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/api/usuarios/{usuarioId}",
+		Metodo:             http.MethodPut,
+		Funcao:             controllers.AtualizarUsuario,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/api/usuarios/{usuarioId}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarUsuario,
+		RequerAutenticacao: true,
+	},
+}
