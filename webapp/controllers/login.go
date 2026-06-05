5bd4518f6af013b639e757dc8f5ecbe2854622b5
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-06-05 19:17:14 -0300

    created the URL of API with `.env`

--- webapp/controllers/login.go
+++ webapp/controllers/login.go
@@ -6,28 +6,19 @@ import (
 	"fmt"
 	"net/http"
 	"webapp/src/config"
+	"webapp/src/modelos"
 	"webapp/src/respostas"
 )
 
-func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+func FazerLogin(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
 
-	nome := r.FormValue("nome")
 	email := r.FormValue("email")
-	nick := r.FormValue("nick")
 	senha := r.FormValue("senha")
-	confirmarSenha := r.FormValue("confirmar_senha")
-
-	if senha != confirmarSenha {
-		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: "As senhas não conferem"})
-		return
-	}
 
 	usuario, erro := json.Marshal(map[string]string{
-		"nome":  nome,
 		"email": email,
-		"nick":  nick,
 		"senha": senha,
 	})
 
@@ -36,7 +27,7 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	url := fmt.Sprintf("%s:%d/api/usuarios", config.API_URL, config.API_PORTA)
+	url := fmt.Sprintf("%s:%d/login", config.API_URL, config.API_PORTA)
 	response, erro := http.Post(url, "application/json", bytes.NewBuffer(usuario))
 
 	if erro != nil {
@@ -51,6 +42,18 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	respostas.JSON(w, http.StatusCreated, nil)
+	var dadosAutenticacao modelos.DadosAutenticacao
+
+	if erro := json.NewDecoder(response.Body).Decode(&dadosAutenticacao); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	usuarioID := dadosAutenticacao.ID
+	token := dadosAutenticacao.Token
+
+	fmt.Println(response.StatusCode, usuarioID, token)
+
+	respostas.JSON(w, http.StatusOK, nil)
 
 }
