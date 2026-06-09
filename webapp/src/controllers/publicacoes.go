a2da808185c1d42131c55dd941b86be8d0241b4e
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-06-08 23:22:16 -0300

    moved controllers to src

---
+++ webapp/src/controllers/publicacoes.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"webapp/src/config"
+	"webapp/src/requisicoes"
+	"webapp/src/respostas"
+)
+
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	publicacao, erro := json.Marshal(map[string]string{
+		"titulo":   r.FormValue("titulo"),
+		"conteudo": r.FormValue("conteudo"),
+	})
+
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	url := fmt.Sprintf("%s:%d/api/publicacoes", config.API_URL, config.API_PORTA)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodPost, url, bytes.NewBuffer(publicacao))
+
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeErro(w, response)
+		return
+	}
+
+	respostas.JSON(w, response.StatusCode, nil)
+
+}
